8a8dfeac0c71618537ad8175b9e453597dd8082e
Сергей Прасолов <prasol555555@gmail.com>
2024-06-16 13:21:41 +0300

    step 2

--- bin/index.js
+++ bin/index.js
@@ -8,6 +8,6 @@ program
   .name('genDiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format');
-
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
